987dc27f1f3e204783af9b172e9636188f49bf02
musaleprithvi <musaleprithvi@gmail.com>
2025-01-10 00:40:36 +0530

    changed info

--- src/modules/Hero.jsx
+++ src/modules/Hero.jsx
@@ -9,20 +9,15 @@ const Hero = () => {
         <div className="row justify-content-center">
           <div className="col-md-4">
             <Profile
-              name="Shyam"
-              title="Web developemtn"
-              profilepic="./profile.jpeg"
+              name="Prithvi Musale"
+              title="Web development"
+              profilepic=""
             ></Profile>
           </div>
           <div className="col-md-6">
-            CEO and Founder of Springevening Private Limited, Bagalkot I am a
-            technology startup founder with a focus on creating hyper-local
-            solutions that make a real impact in communities. With a background
-            in software development and technology, particularly in the media
-            industry, I have a deep understanding of the latest trends and
-            opportunities in the field. I have a proven track record of
-            successfully leading a team and bring a wealth of experience in
-            product development, marketing, and advertising.
+            I am Student of Basaveshwar Engineering College, 
+            I am Pursuing MCA affiliated to VTU university.
+           
           </div>
         </div>
         {/* <ItemList items={items} /> */}
